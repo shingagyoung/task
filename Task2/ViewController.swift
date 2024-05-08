31944246103fff1a882175ba027dea933d7079c1
skia mac mini <skiamacmini@skias-Mac-mini.local>
2024-05-08 09:38:16 +0900

    Init

---
+++ Task2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task2
+//
+//  Created by skia mac mini on 5/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Task2UITests/Task2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Task2UITestsLaunchTests.swift
+//  Task2UITests
+//
+//  Created by skia mac mini on 5/7/24.
+//
+
+import XCTest
+
+final class Task2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
