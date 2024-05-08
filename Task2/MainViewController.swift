039077cbf852d299f726f0276cc28503f5d5832e
gg <gagyoung.shin@skia.kr>
2024-05-08 15:13:31 +0900

    ViewController 파일 및 클래스명 변경

--- Task2/MainViewController.swift
+++ Task2/MainViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
+final class MainViewController: UIViewController {
 
     private let viewModel: MainViewModel = MainViewModel()
     
